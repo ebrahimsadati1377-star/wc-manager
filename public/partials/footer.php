71e4c598570ec7ed2551f2d6852d0f7361d9f68c
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-09-04 10:57:48 +0330

    Load Basalam category search enhancement

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,5 +1,6 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/basalam-category-search.js?v=20260904-1"></script>
 <script>window.CSRF_TOKEN = "<?= csrfToken() ?>";</script>
 <div class="toast-container position-fixed bottom-0 end-0 p-3" id="toastContainer">
   </div>
